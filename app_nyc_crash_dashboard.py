057778a2b9e0c395f3fe3394d5caa2f9401ecac5
lama <lama.hassan@student.giu-uni.de>
2025-11-21 19:05:10 +0200

    Auto-select available port for app run
    Changed the port to 0 for auto-selecting an available port.

--- app_nyc_crash_dashboard.py
+++ app_nyc_crash_dashboard.py
@@ -1189,4 +1189,4 @@ def clear_all_filters(n_clicks):
     )
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8052)), debug=False)
+    app.run(host='0.0.0.0', port=0, debug=False)  # 0 = auto-select available port
